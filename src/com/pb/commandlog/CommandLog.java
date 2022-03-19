9c0898c21f3d515f9bbffab45aab77d78eea1661
wmp71 <100664331+wmp71@users.noreply.github.com>
2022-03-20 07:46:54 +0900

    .

---
+++ src/com/pb/commandlog/CommandLog.java
@@ -0,0 +1,53 @@
+package com.pb.commandlog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CommandLog extends JavaPlugin implements Listener {
+FileConfiguration config = this.getConfig();
+	
+	@Override
+    public void onEnable() {
+		config.options().copyDefaults(true);
+		this.saveConfig();
+		
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+		Player player = event.getPlayer();
+		if(player == null) return;
+		String msg = event.getMessage().replace("/", "");
+		
+		Date currentDate = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("kk:mm:ss");
+		String time = formatter.format(currentDate);
+		
+		String format = config.getString("format");
+		format = format.replaceAll("[$]TIME", time);
+		format = format.replaceAll("[$]WORLD", player.getLocation().getWorld().getName());
+		format = format.replaceAll("[$]USERNAME", player.getName());
+		format = format.replaceAll("[$]COMMAND", msg);
+		
+		Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', format));
+	}
+
+    @Override
+    public void onDisable() {
+		
+    }
+}
